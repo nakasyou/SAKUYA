00d76cca0a91bdfaa16ecff5085099de0beef07a
nakasyou <nakasyou1103@gmail.com>
2023-02-08 18:57:42 +0900

    add background script

--- core/background.js
+++ core/background.js
@@ -1,5 +1,27 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if(message==='get-list'){
-      sendResponse("aad");
+        
+        chrome.storage.local.get(["blacklist"])
+        .then(data=>{
+            return data.blacklist
+        }).then(data=>{
+            if(!data){
+                return updateList();
+            }else{
+                return;
+            }
+        }).then(data=>{
+            sendResponse("aad");
+        })
+        updateList();
+
+        
     }
+    return true;
 });
+
+async function updateList(){
+    const res=await fetch("https://raw.githubusercontent.com/nakasyou/SAKUYA/main/data/black-list.json");
+    const json=await res.json();
+    await chrome.storage.local.set({blacklist:json});
+}
