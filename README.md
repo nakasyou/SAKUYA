d6c66a7fbb65bfb3f8edabb4c0f12030f6748c12
nakasyou <nakasyou1103@gmail.com>
2023-02-12 18:44:07 +0900

    change to typescipt

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # SAKUYA
 ![Logo](https://raw.githubusercontent.com/nakasyou/SAKUYA/main/assets/SAKUYA-logo.svg)  
 SAKUYA is a Chromium's ad blocker.
+## Build
+```shell
+npx tsc --watch
+```
 ## License
 [MIT](https://github.com/nakasyou/SAKUYA/blob/main/LICENSE)
---
+++ addons/element-deleter/main.ts
@@ -0,0 +1,24 @@
+export default async function({list}){
+    const regex_id=new RegExp(list.black_element.id.reduce((a,b)=>{
+        if(a===""){
+            a+=b;
+        }else{
+            a+=`|(${b})`
+        }
+        return a;
+    },""));
+    console.log(regex_id)
+    setInterval(e=>{
+        document.querySelectorAll("div").forEach(elem=>{
+            console.log()
+            if(elem.id.match(regex_id)?.[0]===elem.id){
+                const parent=elem.parentElement;
+                if(parent.childNodes.length===1){
+                    parent.remove();
+                }else{
+                    elem.remove();
+                }
+            }
+        })
+    },100);
+}
